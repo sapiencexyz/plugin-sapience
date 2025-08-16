0e9fd59af2f26979f7a40ef55ae22168e8205691
Sunny Vempati <5723490+sunnyvempati@users.noreply.github.com>
2025-08-16 11:58:25 -0500

    add dependency

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
-import { type IAgentRuntime, type Plugin, logger } from "@elizaos/core";
-import { callToolAction } from "./actions/callToolAction";
-import { readResourceAction } from "./actions/readResourceAction";
-import { simulateTransactionAction } from "./actions/simulateTransactionAction";
-import { submitTransactionAction } from "./actions/submitTransactionAction";
-import { provider } from "./provider";
-import { SapienceService } from "./service";
+import { type IAgentRuntime, type Plugin, logger } from '@elizaos/core';
+import { callToolAction } from './actions/callToolAction';
+import { readResourceAction } from './actions/readResourceAction';
+import { simulateTransactionAction } from './actions/simulateTransactionAction';
+import { submitTransactionAction } from './actions/submitTransactionAction';
+import { provider } from './provider';
+import { SapienceService } from './service';
 
 const sapiencePlugin: Plugin = {
-  name: "sapience",
-  description: "Plugin for connecting to the Sapience API",
+  name: 'sapience',
+  description: 'Plugin for connecting to the Sapience API',
 
   init: async (_config: Record<string, string>, _runtime: IAgentRuntime) => {
-    logger.info("Initializing Sapience plugin...");
+    logger.info('Initializing Sapience plugin...');
   },
 
   services: [SapienceService],
-  actions: [callToolAction, readResourceAction, simulateTransactionAction, submitTransactionAction],
+  actions: [
+    callToolAction,
+    readResourceAction,
+    simulateTransactionAction,
+    submitTransactionAction,
+  ],
   providers: [provider],
+  dependencies: ['@elizaos/plugin-sql'],
 };
 
 export type { SapienceService };
